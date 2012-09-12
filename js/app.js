29f3ba7891ff1d4eed988ba61a6ff18d30748246
Jace Ju <jaceju@gmail.com>
2012-09-12 16:21:49 +0800

    complete app logic

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 define([
-], function () {
+    'models/Status',
+    'views/Switch',
+    'views/Display'
+], function (Status, Switch, Display) {
     return {
         run: function () {
+            var status_model = new Status();
+            var switch_view = new Switch({
+                model: status_model,
+                el: '.input'
+            });
+            var display_view = new Display({
+                model: status_model,
+                el: '.output'
+            });
         }
     }
 });
