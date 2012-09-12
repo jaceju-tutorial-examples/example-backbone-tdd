7e475227754dad2f47aef8b67c1d8852910efd7a
Jace Ju <jaceju@gmail.com>
2012-09-12 11:04:33 +0800

    update description

--- js/spec/SampleSpec.js
+++ js/spec/SampleSpec.js
@@ -4,7 +4,7 @@ define([
     'views/Display'
 ], function (Status, Switch, Display) {
 
-    describe("Status", function() {
+    describe("只有狀態物件", function() {
 
         var status_model;
 
@@ -12,14 +12,14 @@ define([
             status_model = new Status();
         });
 
-        it('變更 status_model 的狀態', function () {
+        it('當改變狀態物件的 music 值後，重新取得的值應該是設定後的值', function () {
             expect(status_model.get('music')).toEqual(false);
             status_model.set('music', true);
             expect(status_model.get('music')).toEqual(true);
         });
     });
 
-    describe("Status + Switch", function() {
+    describe("透過輸入改變狀態物件", function() {
 
         var status_model;
         var switch_view;
@@ -34,9 +34,10 @@ define([
             switch_dom = $('#switch');
         });
 
-        it('透過 switch 變更 status_model 的狀態', function () {
-            expect(status_model.get('music')).toEqual(false);
+        it('在按下兩次 switch 開關時，狀態物件的 music 值應該會改變兩次', function () {
             setTimeout(function () {
+                expect(status_model.get('music')).toEqual(false);
+
                 switch_dom.trigger('click');
                 expect(status_model.get('music')).toEqual(true);
 
@@ -46,7 +47,7 @@ define([
         });
     });
 
-    describe("Status + Display", function () {
+    describe("透過狀態物件改變輸出", function () {
         var status_model;
         var display_view;
         var display_dom;
@@ -60,7 +61,7 @@ define([
             display_dom = $('#status');
         });
 
-        it('用文字顯示 status', function () {
+        it('當狀態物件的 music 改變值時， display 的文字應該要顯示對應的 on 或 off', function () {
             status_model.set('music', false);
             console.log(display_dom.text());
             expect(display_dom.text()).toEqual('off');
@@ -71,7 +72,7 @@ define([
         });
     });
 
-    describe("Status + Display", function () {
+    describe("透過輸入改變輸出", function () {
 
         var status_model;
         var switch_view;
@@ -93,7 +94,7 @@ define([
             display_dom = $('#status');
         });
 
-        it('隱藏 status_model', function () {
+        it('在按下兩次 switch 開關時， display 的文字應該要顯示對應的 on 或 off', function () {
             setTimeout(function () {
                 switch_dom.trigger('click');
                 expect(display_dom.text()).toEqual('off');
